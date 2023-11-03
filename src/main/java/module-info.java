60a5f782cbe8b09d6fe8dc57b6ca2360dac28039
Kupszi <f.farkas.erik998@gmail.com>
2023-11-03 16:06:29 +0100

    import Java.SQL module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,6 +7,7 @@ module com.example.progkornyjavafxuni {
     requires com.dlsc.formsfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
 
     opens com.example.progkornyjavafxuni to javafx.fxml;
     exports com.example.progkornyjavafxuni;
