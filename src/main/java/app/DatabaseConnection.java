1e337410e5339150b9bc0c3cedcfe0e28239b06c
Kupszi <f.farkas.erik998@gmail.com>
2023-11-03 16:06:07 +0100

    database connection

---
+++ src/main/java/app/DatabaseConnection.java
@@ -0,0 +1,25 @@
+package app;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseConnection {
+    public Connection dbLink;
+
+    public Connection getConnection(){
+        String dbName ="javaapp";
+        String dbUser ="root";
+        String dbPassword ="";
+        String url = "jdbc:mysql://localhost/" + dbName;
+
+        try {
+            Class.forName("app.mysql.cj.jdbc.Driver");
+            dbLink=DriverManager.getConnection(url,dbUser,dbPassword);
+
+        } catch (Exception e){
+            e.printStackTrace();
+            e.getCause();
+        }
+        return dbLink;
+    }
+}
