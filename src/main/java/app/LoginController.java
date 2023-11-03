075dc4871ad3a2e320cfd6fc38af88f4055079a2
Kupszi <f.farkas.erik998@gmail.com>
2023-11-03 14:37:19 +0100

    checking for blank fields at login screen

--- src/main/java/app/LoginController.java
+++ src/main/java/app/LoginController.java
@@ -1,20 +1,54 @@
 package app;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 
-public class LoginController {
+import java.io.File;
+import java.util.ResourceBundle;
+import java.net.URL;
+
+public class LoginController implements Initializable {
     @FXML
     private Label loginErrorMessageLabel;
     @FXML
     private Button cancelButton;
-
     @FXML
-    protected void onCancelButtonClick(ActionEvent event) {
+    private Button loginButton;
+    @FXML
+    private TextField usernameTextField;
+    @FXML
+    private TextField passwordTextField;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
+
+    public void onCancelButtonClick(ActionEvent event) {
         Stage stage = (Stage) cancelButton.getScene().getWindow();
         stage.close();
+    }
+    public void onLoginButtonClick(ActionEvent event) {
+        if (!passwordTextField.getText().isBlank() && !usernameTextField.getText().isBlank()){
+            validateLogin(usernameTextField.getText(),passwordTextField.getText());
+        } else {
+            if (passwordTextField.getText().isBlank() && usernameTextField.getText().isBlank()) {
+                loginErrorMessageLabel.setText("Blank Password and Password field.");
+            }
+            else if (usernameTextField.getText().isBlank()){
+                loginErrorMessageLabel.setText("Blank Username field.");
+            }
+            else {
+                loginErrorMessageLabel.setText("Blank Password field.");
+            }
+        }
+    }
+    public void validateLogin(String username, String password){
+        loginErrorMessageLabel.setText("Invalid Username or Password. Please try again!");
     }
 }
